93a42cd4f4ca5f3a52b5c10ac65801e3053fe854
GeorgiEnev <gedev66@gmail.com>
2026-06-22 01:46:52 +0300

    Read directory and search input

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter a directory: ");
+string dir = Console.ReadLine();
+
+if (!Directory.Exists(dir))
+{
+    throw new DirectoryNotFoundException($"Directory does not exist: { dir }");
+}
+
+Console.Write("Enter text to search: ");
+string input = Console.ReadLine();
+
+
